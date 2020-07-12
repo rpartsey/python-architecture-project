d5f7d087e6b70cceda862f649214a1eddd5eea57
rpartsey <partsey2412@gmail.com>
2020-07-12 12:35:20 +0300

    updated constants.py and ui.py

--- streamlit-ui/constants.py
+++ streamlit-ui/constants.py
@@ -1,4 +1,9 @@
-# fastapi endpoint
-FASTAPI_URL: str = 'http://ml-api:8000'
+# ml-api endpoint
+ML_API_URL: str = 'http://localhost:8000'  # 'http://ml-api:8000'
 SEGMENTATION_ENDPOINT: str = 'segmentation'
+
+# metrics-api endpoint
+METRICS_API_URL: str = 'http://localhost:8001'  # 'http://metrics-api:8001'
+IOU_ENDPOINT: str = 'iou'
+
 MASK_SHAPE = (256, 256)
--- streamlit-ui/ui.py
+++ streamlit-ui/ui.py
@@ -3,8 +3,9 @@ import cv2
 import numpy as np
 from PIL import Image
 import streamlit as st
+import st_state_patch # noqa
 
-from constants import FASTAPI_URL, SEGMENTATION_ENDPOINT, MASK_SHAPE
+from constants import ML_API_URL, SEGMENTATION_ENDPOINT, MASK_SHAPE, IOU_ENDPOINT, METRICS_API_URL
 from utils import bytes_to_array, encode, decode, overlay
 
 
@@ -15,33 +16,60 @@ st.write(
     Obtain semantic segmentation maps of the satellite image via U-Net implemented in PyTorch.
     This streamlit example uses a Flask service as backend.
     '''
-) # description and instructions
+)  # description and instructions
 
-binary_file = st.file_uploader('insert image')  # image upload widget
+binary_image = st.file_uploader('Insert image')  # image upload widget
 
-if st.button('Get segmentation map'):
-    if binary_file is None:
+state = st.State()
+if not state:
+    state.segmentation_button_clicked = False
+
+if st.button('Get segmentation map') or state.segmentation_button_clicked:
+    state.segmentation_button_clicked = True
+
+    if binary_image is None:
         st.write("Insert an image!")  # handle case with no image
     else:
-        image = bytes_to_array(binary_file)
+        image = bytes_to_array(binary_image)
         encoded_image = encode(image)
 
-        server_url = f'{FASTAPI_URL}/{SEGMENTATION_ENDPOINT}'
+        ml_api_server_url = f'{ML_API_URL}/{SEGMENTATION_ENDPOINT}'
         r = requests.post(
-            server_url,
+            ml_api_server_url,
             data=encoded_image,
         )
 
         encoded_mask = r.content
-        mask = decode(encoded_mask, dtype=np.uint8).reshape(MASK_SHAPE)
+        mask_pr = decode(encoded_mask, dtype=np.uint8).reshape(MASK_SHAPE)
 
         b, g, r, nir = image
         rgb = np.array([r, g, b]).transpose((1, 2, 0)) # C x H x W -> H x W x C
         rgb = cv2.normalize(rgb, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 
-        masked = overlay(rgb, mask)
+        masked = overlay(rgb, mask_pr)
 
         rgb_image = Image.fromarray(rgb)
         masked_image = Image.fromarray(masked)
 
         st.image([rgb_image, masked_image], width=300)
+
+        binary_mask = st.file_uploader('Insert ground truth mask')
+
+        if st.button('Compare with ground truth'):
+            if binary_mask is None:
+                st.write("Insert an image!")  # handle case with no image
+            else:
+                mask_gt = bytes_to_array(binary_mask)[0]
+                encoded_image = encode(np.stack([mask_gt, mask_pr]))
+
+                image_gt = Image.fromarray(mask_gt * 255)
+                image_pr = Image.fromarray(mask_pr * 255)
+
+                st.image([image_gt, image_pr], width=300)
+
+                metrics_api_server_url = f'{METRICS_API_URL}/{IOU_ENDPOINT}'
+                r = requests.post(
+                    metrics_api_server_url,
+                    data=encoded_image,
+                )
+                st.write(f'IoU score: {r.json()["IoU"]}')
