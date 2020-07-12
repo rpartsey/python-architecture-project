8343b533e7f8cbd62d6db69fae6156592b8c428c
rpartsey <partsey2412@gmail.com>
2020-07-12 12:56:28 +0300

    updated api urls

--- streamlit-ui/constants.py
+++ streamlit-ui/constants.py
@@ -1,9 +1,9 @@
 # ml-api endpoint
-ML_API_URL: str = 'http://localhost:8000'  # 'http://ml-api:8000'
+ML_API_URL: str = 'http://ml-api:8000'
 SEGMENTATION_ENDPOINT: str = 'segmentation'
 
 # metrics-api endpoint
-METRICS_API_URL: str = 'http://localhost:8001'  # 'http://metrics-api:8001'
+METRICS_API_URL: str = 'http://metrics-api:8001'
 IOU_ENDPOINT: str = 'iou'
 
 MASK_SHAPE = (256, 256)
